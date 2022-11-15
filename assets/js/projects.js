8abb3849449c066f34493e6df5a1027db86bf131
Nicholas Yeung <65357530+hwacc-y@users.noreply.github.com>
2022-11-15 16:45:54 -0600

    Initial portfolio config
    Edited the portfolio template from https://github.com/abhn/portfolio

---
+++ assets/js/projects.js
@@ -0,0 +1,108 @@
+$(document).ready(() => {
+    render_projects('featured');
+})
+
+
+let render_projects = (slug) => {
+    let projects_area = $('.projects-wrapper');
+
+    $('.white-button').removeClass('white-button-hover');
+    $(`#${slug}`).addClass('white-button-hover');
+
+    let projects_obj = [
+        {
+            image: 'assets/images/mentors.jpg',
+            link: 'https://github.com/hwacc-y/Tangerine',
+            title: 'Tangerine',
+            demo: false,
+            technologies: ['Python', 'Excel', 'Discord'],
+            description: "A purchase order handling bot that updates members of the Car team on their purchased item status",
+            categories: ['data_e']
+        },
+        {
+            image: 'assets/images/mobile-landscape.jpg',
+            link: 'https://github.com/hwacc-y/MkeyCMkeyClass',
+            title: 'Simian Facial Classifer',
+            demo: false,
+            technologies: ['Deep Learning', 'Julia'],
+            description: "",
+            categories: ['featured', 'data_a']
+        },
+        {
+            image: 'assets/images/collage.jpg',
+            link: 'https://github.com/hwacc-y/stat_207_final',
+            title: 'Spotify Frequency Analysis',
+            demo: false,
+            technologies: ['Data Analysis', 'OpenCV'],
+            description: "",
+            categories: ['featured', 'data_a']
+        },
+        {
+            image: 'assets/images/pi-cloud.jpg',
+            link: 'hwacc-y.github.io',
+            title: 'Modding the WhiteFox Keyboard',
+            demo: false,
+            technologies: ['Soldering'],
+            description: "Modded the 65% WhiteFox Keyboard to hotswappable, installed LEDs, replaced the switches and lubed the stabilizers.",
+            categories: ['diy']
+        },
+        {
+            image: 'assets/images/koalamate.png',
+            link: 'https://github.com/abhn/koalamate',
+            title: 'STORM',
+            demo: false,
+            technologies: ['OSIER', 'python','Genetic Algorithm'],
+            description: "A engery optimization modelling program used for the modelling of routine egg consumption and restocking",
+            categories: ['research', 'diy','featured']
+        },
+    ]
+
+    let projects = [];
+    if(slug == 'all') {
+        projects = projects_obj.map(project_mapper);
+    } 
+    else {
+        projects = projects_obj.filter(project => project.categories.includes(slug)).map(project_mapper);
+    }
+    projects_area.hide().html(projects).fadeIn();
+}
+
+let project_mapper = project => {
+    return `
+        <div class="wrapper">
+                
+            <div class="card radius shadowDepth1">
+
+                ${project.image ? 
+                    `<div class="card__image border-tlr-radius">
+                        <a href="${project.link}">
+                            <img src="${project.image}" alt="image" id="project-image" class="border-tlr-radius">
+                        </a>
+                    </div>`           
+                : ''}
+
+        
+                <div class="card__content card__padding">
+        
+                    <article class="card__article">
+                        <h2><a href="${project.link}">${project.title}</a></h2>
+        
+                        <p class="paragraph-text-normal">${project.description} ${project.demo ? `<a href="${project.demo}">Demo</a>` : ''}</p>
+                    </article>
+
+                                
+                    <div class="card__meta">
+                        ${project.technologies.map(tech =>
+                            `<span class="project-technology paragraph-text-normal">${tech}</span>`
+                        ).join('')}
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    `
+}
+
+let selected = (slug) => {
+    render_projects(slug);
+}
